b2c0fb627ea836922ec82fd8b0ccc9402abcc99a
Asilbek Suvonov <aslwexking777@gmail.com>
2025-08-05 15:34:43 +0500

    dsadsa

--- src/components/ExploreMenu.jsx
+++ src/components/ExploreMenu.jsx
@@ -2,28 +2,44 @@ import React from "react";
 import { menu_list } from "../assets/frontend_assets/assets";
 import { div } from "framer-motion/client";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div>
-      <h1 className="text-center text-2xl">Explore Menu</h1>
-      <p className="text-blue-900">
+      <h1 className=" text-[24px] font-medium">Explore our menu</h1>
+      <p className="w-[710px] mt-2 mb-10">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente ipsam
-        vero, tempora voluptas ea in.
-        <div className="flex justify-between text-center">
-          {menu_list.map((items, inx) => {
-            return (
-              <div key={inx}>
-                <img
-                  className="mt-10"
-                  src={items.menu_image}
-                  alt={items.menu_name}
-                />
-                <p className="mt-2 text-gray-500">{items.menu_name}</p>
-              </div>
-            );
-          })}
-        </div>
+        vero, tempora voluptas ea in. Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit.
       </p>
+      <div className="flex justify-between text-center ">
+        {menu_list.map((items, inx) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === items.menu_name ? "All" : items.menu_name
+                )
+              }
+              key={inx}
+            >
+              <img
+                src={items.menu_image}
+                alt={items.menu_name}
+                className={`
+   rounded-full p-[2px]
+    border-[3px] transition-all duration-300 cursor-pointer
+    ${
+      category === items.menu_name
+        ? "border-orange-500 scale-97"
+        : "border-transparent"
+    }
+  `}
+              />
+              <p className="mt-2 text-gray-500">{items.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react"
 import ExploreMenu from "../../components/ExploreMenu"
 import Header from "../../components/Header"
 import Navbar from "../../components/Navbar"
 
 const Home = () => {
+  const [category, setCategory]=useState("All")
   return (
     <div className="container">
       <Navbar/>
       <Header/>
-      <ExploreMenu/>
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   )
 }
