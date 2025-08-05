d7de75bb56a3122495f5a5bbcc887d09c80132d5
Asilbek Suvonov <aslwexking777@gmail.com>
2025-08-05 22:48:42 +0500

    add

--- src/components/LoginPopup.jsx
+++ src/components/LoginPopup.jsx
@@ -5,44 +5,61 @@ const LoginPopup = ({ setShowLogin }) => {
   const [currState, setCurrState] = useState("Sign Up");
 
   return (
-    <div>
-      <form>
-        <div>
-          <h2>{currState}</h2>
+    <div
+      onClick={() => setShowLogin(false)}
+      className="fixed z-200 bg-[#4d4d4d48] w-full h-[100vh] top-0 left-0"
+    >
+      <form className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white flex flex-col py-7 px-8 w-[400px] h-fit z-50 shadow-md rounded-xl">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-xl font-semibold ">{currState}</h2>
           <img
             onClick={() => setShowLogin(false)}
             src={icons.cross_icon}
             alt=""
+            className="w-[15px] cursor-pointer"
           />
         </div>
-        <div>
+        <div className="flex flex-col gap-4">
           {currState === "Login" ? (
             <></>
           ) : (
             <input type="text" placeholder="Your name"></input>
           )}
-          <input type="text" placeholder="Your name " required />
           <input type="email" placeholder="Your email" required />
           <input type="password" placeholder="Password" required />
         </div>
-        <button>
+        <button className="bg-orange-600 py-2 px-0 rounded-md text-white mt-4">
           {currState === "Sign Up" ? "Create account" : "Login  "}
         </button>
-        <div>
-          <input type="checkbox" required />
-          <p>
-            {" "}
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum
-            repellendus rem{" "}
-          </p>
+        <div className="flex items-center gap-2 justify-start mt-4">
+          <input
+            type="checkbox"
+            className="w-4 h-4 accent-orange-500 cursor-pointer "
+            required
+          />
+          <p className="text-[13px]">Lorem, ipsum dolor sit amet consectetur</p>
         </div>
         {currState === "Login" ? (
-          <p>
-            Create new account <span onClick={()=>setCurrState("Sogn Up")}>Click here</span>
+          <p className="text-xs text-center mt-4">
+            Create new account
+            <span
+              onClick={() => setCurrState("Sign Up")}
+              className="text-orange-500"
+            >
+              {" "}
+              Click here
+            </span>
           </p>
         ) : (
-          <p>
-            Already have an account? <span onClick={()=>setCurrState(Login)}>Login here</span>
+          <p className="text-xs text-center mt-4">
+            Already have an account?
+            <span
+              onClick={() => setCurrState("Login")}
+              className="text-orange-500"
+            >
+              {" "}
+              Login here
+            </span>
           </p>
         )}
       </form>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,31 +3,35 @@ import { assets } from "../assets/admin_assets/assets";
 import { icons } from "../assets/frontend_assets/assets";
 import { Link } from "react-router-dom";
 
-const Navbar = ({setShowLogin}) => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
   return (
     <div className="flex justify-between items-center container py-[20px]">
       <img src={icons.logo} alt="" className="w-[120px]" />
       <ul className="nav flex justify-between gap-8">
-        <Link to='/'
+        <Link
+          to="/"
           onClick={() => setMenu("Home")}
           className={menu === "Home" ? "active" : ""}
         >
           Home
         </Link>
-        <a href="#explore-menu"
+        <a
+          href="#explore-menu"
           onClick={() => setMenu("Menu")}
           className={menu === "Menu" ? "active" : ""}
         >
           Menu
         </a>
-        <a href="#app-download"
+        <a
+          href="#app-download"
           onClick={() => setMenu("Mobile-app")}
           className={menu === "Mobile-app" ? "active" : ""}
         >
           Mobile-app
         </a>
-        <a href="#footer"
+        <a
+          href="#footer"
           onClick={() => setMenu("Contact-Us")}
           className={menu === "Contact-Us" ? "active" : ""}
         >
@@ -49,7 +53,10 @@ const Navbar = ({setShowLogin}) => {
           />
           <div className="absolute min-w-[10px] min-h-[10px] bg-red-500 rounded-4xl top-[4px] right-[-5px]"></div>
         </div>
-        <button onClick={()=>setShowLogin(true)} className="transparent border border-4-[1px] py-2 px-6 rounded-4xl text-[15px] cursor-pointer hover:bg-orange-100 transition duration-300">
+        <button
+          onClick={() => setShowLogin(true)}
+          className="transparent border border-4-[1px] py-2 px-6 rounded-4xl text-[15px] cursor-pointer hover:bg-orange-100 transition duration-300"
+        >
           Sign in
         </button>
       </div>
