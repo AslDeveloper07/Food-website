3d6f559391306e2fa196bea46896e030a62a2421
Asilbek Suvonov <aslwexking777@gmail.com>
2025-08-05 18:24:41 +0500

    sada

--- src/components/FoodDisplay.jsx
+++ src/components/FoodDisplay.jsx
@@ -5,14 +5,15 @@ import FoodItem from "./FoodItem";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+
   return (
     <div className="" id="food-display">
-      <h2>Top dishes near you</h2>
+      <h2 className="mt-13 mb-4 text-2xl font-semibold">Top dishes near you</h2>
 
-      <div>
+      <div className="container flex-wrap flex gap-6 justify-center">
         {
           food_list.map((food, i)=>{
-            return <FoodItem key={i} id={food._id} name={food.name} description={food.description} price={food.price} image={food.image} />
+            return <FoodItem key={i} id={food.id} name={food.name} description={food.description} price={food.price} image={food.image} />
           })
         }
       </div>
--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../assets/admin_assets/assets";
+import { icons } from "../assets/frontend_assets/assets";
 
-const FoodItem = ({id, name, price, description, image}) => {
+const FoodItem = ({ id, name, price, description, image }) => {
+
+  const [itemCount, setItemCount]=useState(0)
   return (
-    <div>FoodItem</div>
-  )
-}
+    <div className="w-[280px] flex flex-col gap-3 rounded-2xl border border-4-1px border-[#4d4d4d23] overflow-hidden hover:shadow-[0px_3px_8px_0px_rgba(34,60,80,0.2)] cursor-pointer transition duration-300">
+      <div>
+        <img src={image} alt="" className="" />
+      </div>
+      <div className="py-1 px-3 pb-4">
+        <div className="flex justify-between items-center mb-3">
+          <p className="font-samibold">{name}</p>
+          <img className="w-[70px]" src={icons.rating_starts} alt={name} />
+        </div>
+        <p className="text-[13px] mb-4">{description}</p>
+        <p className="text-orange-500 font-semibold">$ {price}</p>
+      </div>
+    </div>
+  );
+};
 
-export default FoodItem
+export default FoodItem;
